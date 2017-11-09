4c48487c8c1c5174370ed69b0d273a7eed7bd6f4
Philip Van Raalte <phil.vanraalte@gmail.com>
2017-11-09 13:08:38 -0500

    Added SwiperJS Slides for Quiz Selection
    -added Swiper JS stylesheet
    -added colours to slides for selecting a quiz
    -improved documentation for SpectreCSS.js
    -added more quizes (Nintendo and Sega)
    -added styles for quiz slides

--- src/components/SpectreCSS.js
+++ src/components/SpectreCSS.js
@@ -1,28 +1,53 @@
-/*
+/** @license GPL-3.0
+ * SpectreCSS.js
+ *
  * @author: Philip Van Raalte
  * @date November 8, 2017
  *
- * Functional components for the Spectre CSS library
+ * Functional components for the Spectre CSS library.
  */
 import React from 'react';
 import _ from 'lodash';
 
+/**
+ * Prepends the className from props to the component default className.
+ * @param {String} defaultClass The default class for the component.
+ * @param {String} newClass The class name to prepend to the default class. (So the default class will override it)
+ * @return {String} JSX Component
+ */
+function addClass(defaultClass, newClass) {
+  return `${_.isString(newClass) ? newClass : ''} ${defaultClass}`.trim();
+}
+
+/**
+ * A divider for separating elements.
+ * @param {Object} props Properties for the component.
+ * @return {XML} JSX Component
+ */
 export const Divider = (props) => {
   let className = "divider";
 
-  if(_.isString(props.className)) {
-    className = `${props.className} ${className}`;
+  // add the className prop to the className
+  className = addClass(className, props.className);
+
+  // allow size to be passed as a string or a number
+  if(!_.isEmpty(props.size) || _.isNumber(props.size)) {
+    className = `${className} col-${props.size.toString().trim()}`;
   }
 
   return <div {...props} className={className}/>;
 };
 
+/**
+ * A page for containing elements.
+ * @param {Object} props Properties for the component.
+ * @return {XML} JSX Component
+ */
 export const Page = (props) => {
   let className = "page container centered text-center";
 
-  if(_.isString(props.className)) {
-    className = `${props.className} ${className}`;
-  }
+  // add the className prop to the className
+  className = addClass(className, props.className);
 
   return <div {...props} className={className}/>;
 };
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -13,9 +13,12 @@ class Home extends Component {
   }
 
   renderSlides() {
-    let slides = quizes.map(({name, questions}) => {
+    let slides = quizes.map(({name, colour}) => {
       return(
-        <Slide key={name}>
+        <Slide key={name} className="quiz-slide" style={{backgroundColor: colour}}>
+          <span>
+            <h3>{name}</h3>
+          </span>
         </Slide>
       );
     });
@@ -28,9 +31,8 @@ class Home extends Component {
           centeredSlides: true,
           grabCursor: true,
           keyboardControl: true
-          //mousewheelControl: true,
-          //mousewheelForceToAxis: true
         }}
+        className="text-light"
       >
         {slides}
       </Swiper>
@@ -41,9 +43,10 @@ class Home extends Component {
     return(
       <Page>
         <h1>Ultra Trivia</h1>
-        <Divider className="col-10 centered"/>
+        <Divider className="centered" size={10}/>
         <div>
           <Link to="/quiz" className="btn btn-lg col-8">Quiz</Link>
+          <br/><br/>
           {this.renderSlides()}
         </div>
       </Page>
