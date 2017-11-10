2a4767da72167e6f10be19b313f22f530f07589f
Philip Van Raalte <phil.vanraalte@gmail.com>
2017-11-10 14:04:19 -0500

    Added show question and answers functionality

--- src/components/pages/Quiz.js
+++ src/components/pages/Quiz.js
@@ -6,30 +6,84 @@ import {Page, Divider, Parallax} from '../SpectreCSS';
 import quizes from '../../quizes';
 
 class Quiz extends Component {
-  render() {
-    let quiz;
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      quiz: null,
+      questionNum: 0
+    };
+
+    this.fetchQuiz = this.fetchQuiz.bind(this);
+  }
+
+  fetchQuiz() {
+    let newQuiz;
     const {quizName} = this.props.match.params;
-    if(!_.isUndefined(quizName) && !_.isEmpty(quizName.toString())) {
-      quiz = _.find(quizes, {uniqueID: quizName});
+    if (!_.isUndefined(quizName) && !_.isEmpty(quizName.toString())) {
+      newQuiz = _.find(quizes, {uniqueID: quizName});
+      if(_.isEmpty(newQuiz)) {
+        return false;
+      } else {
+        setTimeout(() => {this.setState({quiz: newQuiz});});
+      }
     }
+    return true;
+  }
+
+  renderAnswer(answer, key) {
+    return (
+      <div className="column col-6 centered" key={key}>
+        <Parallax
+          topLeft={() => console.log("top left")} topRight={() => console.log("top right")}
+          bottomLeft={() => console.log("bottom left")} bottomRight={() => console.log("bottom right")}
+        >
+          <button type="button" className="btn btn-lg col-12 p-2">{answer}</button>
+        </Parallax>
+      </div>
+    );
+  }
+
+  renderQuestions(quiz) {
+    const {questionNum} = this.state;
+    const {question, answers} = quiz.questions[questionNum];
     console.log("Quiz", quiz);
 
     return(
-      <Page>
-        <h1>{quizName}</h1>
-        <Divider className="centered" size={10}/>
-        <Link to="/" className="btn btn-lg">Home</Link>
-        <br/>
-        {_.isEmpty(quiz) ? <Redirect to="/"/> : null}
-        <br/>
-        <div className="col-4">
-          <Parallax
-            topLeft={() => console.log("top left")} topRight={() => console.log("top right")}
-            bottomLeft={() => console.log("bottom left")} bottomRight={() => console.log("bottom right")}
-          >
-            <button type="button" className="btn btn-lg col-12 p-2">Button 3</button>
-          </Parallax>
+      <div>
+        <h5>{question}</h5>
+        <div className="columns col-gapless">
+          {
+            _.shuffle(answers).map((answer, index) => {
+              return this.renderAnswer(answer, `${questionNum}-${index}`);
+            })
+          }
         </div>
+        <br/>
+        <div>
+          Question {questionNum+1} of {quiz.questions.length}
+        </div>
+      </div>
+    );
+  }
+
+  render() {
+    const {quiz} = this.state;
+
+    if(_.isEmpty(quiz)) {
+      if(!this.fetchQuiz()) {
+        return <Redirect to="/"/>;
+      }
+    }
+
+    return(
+      <Page>
+        <h2>{_.isEmpty(quiz) ? "Loading..." : quiz.name}</h2>
+        <Divider className="centered" size={10}/>
+        <Link to="/" className="btn btn-lg btn-primary">Back to Main Menu</Link>
+        <br/>
+        <br/>
+        {_.isEmpty(quiz) ? "Loading..." : this.renderQuestions(quiz)}
       </Page>
     );
   }
