ba0dd3c6cfd6bfb1830af1a309e29ff37a0f6efd
Philip Van Raalte <phil.vanraalte@gmail.com>
2017-11-10 17:03:50 -0500

    Added Button to SpectreCSS.js

--- src/components/SpectreCSS.js
+++ src/components/SpectreCSS.js
@@ -20,6 +20,47 @@ function addClass(defaultClass, newClass) {
 }
 
 /**
+ * A simple button.
+ * @param {Object} props Properties for the component.
+ * @return {XML} JSX Component
+ */
+export const Button = (props) => {
+  const {small, large, block, primary} = props;
+  let className = "btn";
+
+  // add the className prop to the className
+  className = addClass(className, props.className);
+
+  // allow size to be passed as a string or a number
+  if(!_.isEmpty(props.size) || _.isNumber(props.size)) {
+    className = `${className} col-${props.size.toString().trim()}`;
+  }
+
+  if(large) {
+    className = `${className} btn-lg`;
+  } else if(small) {
+    className = `${className} btn-sm`;
+  }
+
+  if(block) {
+    className = `${className} btn-block`;
+  }
+
+  if(primary) {
+    className = `${className} btn-primary`;
+  }
+
+  // remove unnecessary props
+  let myProps = _.omit(props, ['small', 'large']);
+  // if type is not specified default to type button
+  if(_.isEmpty(myProps.type)) {
+    myProps.type = "button";
+  }
+
+  return <button {...props} className={className}/>;
+};
+
+/**
  * A divider for separating elements.
  * @param {Object} props Properties for the component.
  * @return {XML} JSX Component
@@ -52,15 +93,20 @@ export const Page = (props) => {
   return <div {...props} className={className}/>;
 };
 
+/**
+ * A hover parallax effect.
+ * @param {Object} props Properties for the component.
+ * @return {XML} JSX Component
+ */
 export const Parallax = (props) => {
   const {children, title, topLeft, topRight, bottomLeft, bottomRight} = props;
-  let className = "parallax", myProps = {...props};
+  let className = "parallax";
 
   // add the className prop to the className
   className = addClass(className, props.className);
 
   // remove unnecessary props
-  myProps = _.omit(props, ['children', 'title', 'topLeft', 'topRight', 'bottomLeft', 'bottomRight']);
+  let myProps = _.omit(props, ['children', 'title', 'topLeft', 'topRight', 'bottomLeft', 'bottomRight']);
 
   return (
     <div {...myProps} className={className}>
