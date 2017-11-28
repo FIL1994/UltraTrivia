138e646b682d7e1564140173044a350db5699904
Philip Van Raalte <phil.vanraalte@gmail.com>
2017-11-28 10:55:51 -0500

    Added tests w/ Jest and Enzyme

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+/**
+ * @author Philip Van Raalte
+ * @date 2017-11-28
+ */
+module.exports = {
+  "verbose": true,
+  "setupFiles": ["./jest/shim.js", "./jest/setupJest.js"]
+};
---
+++ src/__tests__/Timer.test.js
@@ -0,0 +1,20 @@
+/**
+ * @author Philip Van Raalte
+ * @date 2017-11-28
+ */
+import React from 'react';
+import {shallow} from 'enzyme';
+import Timer from '../components/Timer';
+import _ from 'lodash';
+
+describe("Timer", () => {
+  test("state has a seconds variable that is a number", () => {
+    const timer = shallow(<Timer/>);
+    expect(_.isNumber(timer.state().seconds)).toBe(true);
+  });
+
+  test("starts counting from 0", () => {
+    const timer = shallow(<Timer/>);
+    expect(timer.state().seconds).toBe(0);
+  });
+});
